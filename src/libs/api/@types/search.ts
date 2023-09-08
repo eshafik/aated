fc18cdb97bbfe739b4213e234b77dc5e4c61fa4b
Abu Shyed Hemel <50671938+AbuShyedHemel@users.noreply.github.com>
2023-09-08 13:24:31 +0600

    Search Filter add

---
+++ src/libs/api/@types/search.ts
@@ -0,0 +1,7 @@
+export interface SearchResponse {
+  data?: {
+    id?: string | number;
+    name?: string;
+    batch_number?: string;
+  };
+}
---
+++ src/libs/api/searchAPI.ts
@@ -0,0 +1,23 @@
+import config from "../../config";
+import { authService } from "../auth/auth.service";
+import { SearchResponse } from "./@types/search";
+import { HttpAuthService } from "./httpService/httpAuth.service";
+
+class BatchAPI {
+  constructor(private http: HttpAuthService) {}
+  getBatchList() {
+    return this.http.get<SearchResponse>(
+      "api/v1/core/public/batches?limit=200"
+    );
+  }
+
+  getOccupationList() {
+    return this.http.get<SearchResponse>("api/v1/core/public/occupations/");
+  }
+
+  getJobDepartment() {
+    return this.http.get<SearchResponse>("api/v1/core/public/job-departments/");
+  }
+}
+const httpAuthService = new HttpAuthService(config.apiURL, authService);
+export const batchAPI = new BatchAPI(httpAuthService);
