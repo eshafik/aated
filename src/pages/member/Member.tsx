31a91713b86e9820ce0dc04e4d714b38c131edb1
Abu Shyed Hemel <50671938+AbuShyedHemel@users.noreply.github.com>
2023-09-28 18:59:21 +0600

    Updated Member Profile

--- src/pages/member/Member.tsx
+++ src/pages/member/Member.tsx
@@ -1,6 +1,7 @@
-import { Card, Col, Image, Row, Skeleton, Typography } from "antd";
+import { Avatar, Col, Image, Row, Skeleton, Typography } from "antd";
 import { useQuery } from "react-query";
 import { useParams } from "react-router-dom";
+import { StyledCard } from "../../components/StyleCard";
 import { membersAPI } from "../../libs/api/membersAPI";
 
 const Member = () => {
@@ -12,28 +13,33 @@ const Member = () => {
     <Skeleton loading={isLoading}>
       <Row justify={"center"}>
         <Col span={12}>
-          <Card className="shadow-2xl bg-transparent ">
+          <StyledCard className="shadow-2xl bg-transparent ">
             <div>
-              <Image
-                src={memberData?.data?.profile_pic}
-                className="h-32 w-24 rounded-lg"
-              />
+              {memberData?.data?.profile_pic ? (
+                <Image
+                  src={memberData?.data?.profile_pic}
+                  className="h-32 w-24 rounded-lg"
+                />
+              ) : (
+                <Avatar className="h-32 w-24 rounded-lg" />
+              )}
               <Typography.Title level={3}>
                 {memberData?.data?.name}
               </Typography.Title>
-              <div>{memberData?.data?.student_id}</div>
-              <div>{memberData?.data?.batch?.name}</div>
-              <div>{memberData?.data?.passing_year}</div>
-              <div>{memberData?.data?.email}</div>
-              <div>{memberData?.data?.phone}</div>
-              <div>{memberData?.data?.professional_designation}</div>
-              <div>{memberData?.data?.contact_details}</div>
-              <div>{memberData?.data?.employment_status}</div>
+              <div className="ml-5">
+                <div>{memberData?.data?.contact_details}</div>
+                <div>{memberData?.data?.email}</div>
+                <div>{memberData?.data?.phone}</div>
+                <div>{memberData?.data?.student_id}</div>
+                <div>{memberData?.data?.batch?.name}</div>
+                <div>{memberData?.data?.passing_year}</div>
+                <div>{memberData?.data?.professional_designation}</div>
+              </div>
               <Typography.Title level={4} className="flex mt-5">
                 Experiences
               </Typography.Title>
               {memberData?.data?.experiences?.map((exp, i) => (
-                <div key={i} className="mb-8">
+                <div key={i} className="mb-8 ml-5">
                   <Typography.Title className="mt-0" level={5}>
                     {exp?.designation}
                   </Typography.Title>
@@ -44,7 +50,7 @@ const Member = () => {
                 </div>
               ))}
             </div>
-          </Card>
+          </StyledCard>
         </Col>
       </Row>
     </Skeleton>
