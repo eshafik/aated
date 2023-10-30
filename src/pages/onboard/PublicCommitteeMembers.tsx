87c16addb5ccdd5a9407810b3fce104581c3daeb
Abu Shyed Hemel <50671938+AbuShyedHemel@users.noreply.github.com>
2023-10-30 14:57:58 +0600

    remove comments

--- src/pages/onboard/PublicCommitteeMembers.tsx
+++ src/pages/onboard/PublicCommitteeMembers.tsx
@@ -21,35 +21,6 @@ const PublicCommitteeMembers = () => {
     slidesToShow: 3,
     arrows: true,
     speed: 500,
-    // responsive: [
-    //   {
-    //     breakpoint: 1024,
-    //     settings: {
-    //       slidesToShow: 3,
-    //       slidesToScroll: 3,
-    //       infinite: true,
-    //       dots: true,
-    //     },
-    //   },
-    //   {
-    //     breakpoint: 600,
-    //     settings: {
-    //       slidesToShow: 2,
-    //       slidesToScroll: 2,
-    //       infinite: true,
-    //       dots: true,
-    //     },
-    //   },
-    //   {
-    //     breakpoint: 480,
-    //     settings: {
-    //       slidesToShow: 1,
-    //       slidesToScroll: 1,
-    //       infinite: true,
-    //       dots: true,
-    //     },
-    //   },
-    // ],
   };
 
   return (
