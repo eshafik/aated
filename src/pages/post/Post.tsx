dd9daae392950cf77e3f87d2b04ee0301221c3cc
Abu Shyed Hemel <50671938+AbuShyedHemel@users.noreply.github.com>
2024-09-04 16:47:01 +0600

    post page refactor

--- src/pages/post/Post.tsx
+++ src/pages/post/Post.tsx
@@ -17,6 +17,7 @@ import {
 import TextArea from "antd/es/input/TextArea";
 import { useMutation, useQuery, useQueryClient } from "react-query";
 import { useParams } from "react-router-dom";
+import CardMeta from "../../components/CardMeta";
 import { useUserDetails } from "../../container/RoleProvider";
 import {
   CommentPayload,
@@ -65,128 +66,120 @@ const Post = () => {
   );
 
   return (
-    <Row align={"middle"} justify={"center"}>
-      <Col>
-        <Skeleton loading={isLoading}>
-          <Card
-            className="max-w-2xl"
-            title={
-              <>
+    <Skeleton loading={isLoading}>
+      <div className="flex justify-center p-5">
+        <Card
+          className="max-w-[calc(100vh-200px)] min-w-[calc(100vh-200px)]"
+          title={
+            <CardMeta
+              icon={
                 <Avatar
                   className="mr-2"
                   src={postData?.data?.user?.profile_pic}
                 />
-                {postData?.data?.user?.name}
-              </>
-            }
-            extra={
-              postData?.data?.user?.id === userID ? (
-                <EditPost postsDetails={postData?.data} />
-              ) : (
-                ""
-              )
-            }
-          >
-            <Row justify={"space-between"}>
-              <Typography.Title className="mt-0" level={5}>
-                {postData?.data?.title}
-              </Typography.Title>
-              {formatDate(postData?.data?.created_at)}
-            </Row>
-            {postData?.data?.attachments?.map((pic, i) =>
-              pic ? <Image key={i} alt="example" src={pic} /> : ""
-            )}
-            <div className="mt-4 mb-4">{postData?.data?.body}</div>
-            <Typography.Title level={5}>
-              Comment ({postData?.data?.total_comments})
+              }
+              title={postData?.data?.user?.name}
+            />
+          }
+          extra={
+            postData?.data?.user?.id === userID && (
+              <EditPost postsDetails={postData?.data} />
+            )
+          }
+        >
+          <Row justify={"space-between"}>
+            <Typography.Title className="mt-0" level={5}>
+              {postData?.data?.title}
             </Typography.Title>
+            {formatDate(postData?.data?.created_at)}
+          </Row>
+          {postData?.data?.attachments?.map((pic, i) =>
+            pic ? <Image key={i} alt="example" src={pic} /> : ""
+          )}
+          <div className="mt-4 mb-4">{postData?.data?.body}</div>
+          <Typography.Title level={5}>
+            Comment ({postData?.data?.total_comments})
+          </Typography.Title>
 
-            {postData?.data?.comments?.map((comments, i) => (
-              <Row key={i}>
-                <Col>
-                  <Avatar src={comments?.user?.profile_pic} />
-                </Col>
-                <Col>
-                  <Card
-                    className="bg-slate-100 w-56 ml-2 mt-2"
-                    size="small"
-                    title={
-                      <div className="mt-4">
-                        <Typography.Text>
-                          {comments?.user?.name}
-                        </Typography.Text>
-                        <Typography.Paragraph>
+          {postData?.data?.comments?.map((comments, i) => (
+            <Row key={i}>
+              <Col>
+                <Avatar src={comments?.user?.profile_pic} />
+              </Col>
+              <Col>
+                <Card
+                  className="bg-slate-100 w-56 ml-2 mt-2"
+                  size="small"
+                  title={
+                    <div className="mt-4">
+                      <Typography.Text>{comments?.user?.name}</Typography.Text>
+                      <Typography.Paragraph>
+                        {
+                          <Typography.Paragraph type="secondary">
+                            {formatDate(comments.created_at)}
+                          </Typography.Paragraph>
+                        }
+                      </Typography.Paragraph>
+                    </div>
+                  }
+                  extra={
+                    <Dropdown
+                      menu={{
+                        items: [
                           {
-                            <Typography.Paragraph type="secondary">
-                              {formatDate(comments.created_at)}
-                            </Typography.Paragraph>
-                          }
-                        </Typography.Paragraph>
-                      </div>
-                    }
-                    extra={
-                      <Dropdown
-                        menu={{
-                          items: [
-                            {
-                              key: "member",
-                              label: (
-                                <Popconfirm
-                                  title="Delete Comment"
-                                  description="Are you sure want o delete this Comment?"
-                                  onConfirm={() =>
-                                    mutateDeleteComment({
-                                      comment_id: comments?.id,
-                                      is_active: false,
-                                    })
-                                  }
-                                  okText="Yes"
-                                  cancelText="No"
-                                  okType="danger"
-                                >
-                                  <DeleteOutlined /> Delete
-                                </Popconfirm>
-                              ),
-                            },
-                          ],
-                        }}
-                      >
-                        <Button type="text" icon={<MoreOutlined />} />
-                      </Dropdown>
-                    }
-                  >
-                    {comments?.comment}
-                  </Card>
-                </Col>
-              </Row>
-            ))}
-            <Form
-              form={form}
-              onFinish={(values) => {
-                mutate({
-                  comment: values.comment,
-                  post: slag,
-                });
-              }}
-              className="mt-5"
-            >
-              <Form.Item name="comment">
-                <TextArea rows={3} />
-              </Form.Item>
-              <Form.Item>
-                <Button
-                  type="primary"
-                  loading={loadingComment}
-                  htmlType="submit"
+                            key: "member",
+                            label: (
+                              <Popconfirm
+                                title="Delete Comment"
+                                description="Are you sure want o delete this Comment?"
+                                onConfirm={() =>
+                                  mutateDeleteComment({
+                                    comment_id: comments?.id,
+                                    is_active: false,
+                                  })
+                                }
+                                okText="Yes"
+                                cancelText="No"
+                                okType="danger"
+                              >
+                                <DeleteOutlined /> Delete
+                              </Popconfirm>
+                            ),
+                          },
+                        ],
+                      }}
+                    >
+                      <Button type="text" icon={<MoreOutlined />} />
+                    </Dropdown>
+                  }
                 >
-                  Comment
-                </Button>
-              </Form.Item>
-            </Form>
-          </Card>
-        </Skeleton>
-      </Col>
-    </Row>
+                  {comments?.comment}
+                </Card>
+              </Col>
+            </Row>
+          ))}
+          <Form
+            form={form}
+            onFinish={(values) => {
+              mutate({
+                comment: values.comment,
+                post: slag,
+              });
+            }}
+            className="mt-5"
+          >
+            <Form.Item name="comment">
+              <TextArea rows={3} />
+            </Form.Item>
+            <Form.Item>
+              <Button type="primary" loading={loadingComment} htmlType="submit">
+                Comment
+              </Button>
+            </Form.Item>
+          </Form>
+        </Card>
+      </div>
+    </Skeleton>
   );
 };
 
